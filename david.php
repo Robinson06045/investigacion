1158c546be6eb3178f6bf303ecbbf0fd7f1e9379
davatem17 <perezd625@gmail.com>
2020-08-13 14:54:18 -0500

    Hoja de Vida David

---
+++ david.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>David Perez</h1>
+    <p>Domicilio: Ambato <br>
+        Edad: 22 años
+        Hobbies: videojuegos, peliculas, nopor xd
+    </p>
+</body>
+</html>
